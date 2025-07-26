c637c5180d1c6c946d025a8f8786bdc184795f20
khrisnaa <igdkhrisnaaa@gmail.com>
2025-07-26 13:49:51 +0800

    ui: styling faq section (desktop)

--- resources/js/pages/welcome.tsx
+++ resources/js/pages/welcome.tsx
@@ -1,93 +1,31 @@
-import { Button } from '@/components/ui/button';
+import { FAQ } from '@/components/blocks/faq';
 import { Separator } from '@/components/ui/separator';
-import { MoveDown } from 'lucide-react';
+import HeroSection from '@/components/welcome-page.tsx/hero-section';
 
 export default function Welcome() {
     // const { auth } = usePage<SharedData>().props;
 
     return (
-        <div className="bg-neutral-100 px-12">
-            <section className="flex h-screen overflow-hidden bg-neutral-100">
-                <div className="relative flex flex-1 items-center">
-                    <div className="absolute top-16 left-0 font-noto-jp text-xl font-semibold">
-                        <div className="flex gap-2">
-                            <span>タ</span>
-                            <span>ス</span>
-                            <span>ク</span>
-                            <span>ト</span>
-                        </div>
-                        <div className="flex flex-col gap-2">
-                            <span>ス</span>
-                            <span>ク</span>
-                            <span>ト</span>
-                        </div>
-                    </div>
-                    <div className="absolute bottom-12 left-0 text-xl font-semibold">
-                        <div className="flex gap-2">
-                            <span>タ</span>
-                            <span>ス</span>
-                            <span>ク</span>
-                            <span>ト</span>
-                        </div>
-                    </div>
-                    <div className="h-[50%] translate-y-[20%] bg-red-500">
-                        <Separator orientation="vertical" className="bg-neutral-600" />
-                    </div>
-                    <div className="flex w-full items-center justify-center">
-                        <div className="w-full max-w-xs space-y-4">
-                            <div className="flex flex-col text-6xl font-semibold">
-                                <span>Turn</span>
-                                <span>Tasks</span>
-                                <span>Into</span>
-                                <span>Adventures</span>
-                            </div>
-                            <Separator className="bg-neutral-600" />
-                            <p>Manage your tasks, level up with your team, and make every project feel like a game.</p>
-                        </div>
+        <div>
+            <HeroSection />
+            <section className="h-screen bg-primary px-12 py-8 text-secondary">
+                <div className="flex items-center justify-start">
+                    <div className="flex items-center gap-2">
+                        <Separator />
+                        <span className="font-playfair text-2xl font-medium">Taskto</span>
                     </div>
                 </div>
-                <div className="relative flex-[.6] overflow-hidden">
-                    <div className="absolute top-16 left-1/2 -translate-x-1/2">
-                        <div className="flex items-center gap-2">
-                            <div className="relative size-4">
-                                <img src="/icons/diamonx.svg" alt="Diamond" className="h-full w-full object-cover" />
-                            </div>
-                            <span className="font-playfair text-2xl font-medium">Taskto</span>
-                            <div className="relative size-4">
-                                <img src="/icons/diamonx.svg" alt="Diamond" className="h-full w-full object-cover" />
+                <div className="flex h-full items-center justify-center gap-4">
+                    <div className="flex-[1]">
+                        <div className="flex flex-col text-6xl font-semibold">The spark of idea</div>
+                        <div className="relative flex h-[70dvh] w-full items-center justify-center overflow-hidden">
+                            <div className="relative scale-125 overflow-hidden">
+                                <img src="/assets/monster2-white.png" alt="Monster" className="h-full w-full object-cover" />
                             </div>
                         </div>
                     </div>
-                    <div className="relative h-[160dvh] -translate-y-[8%] overflow-hidden">
-                        <img alt="Home Image" src="/assets/char2.png" className="h-full w-full object-cover object-top" />
-                    </div>
-                    <div className="absolute bottom-0 left-1/2 flex aspect-square w-[60%] -translate-x-1/2 translate-y-[55%] items-start justify-center rounded-full border border-neutral-600 bg-neutral-100 text-primary">
-                        <div className="flex flex-col items-center justify-center p-5">
-                            <span className="text-lg font-medium">FAQ</span>
-                            <MoveDown className="size-4" />
-                        </div>
-                    </div>
-                </div>
-                <div className="relative flex-1">
-                    <div className="absolute top-16 right-0 font-noto-jp text-xl font-semibold">
-                        <div className="flex flex-row-reverse gap-2">
-                            <span>タ</span>
-                            <span>ス</span>
-                            <span>ク</span>
-                            <span>ト</span>
-                        </div>
-                        <div className="flex flex-col items-end gap-2">
-                            <span>ス</span>
-                            <span>ク</span>
-                            <span>ト</span>
-                        </div>
-                    </div>
-
-                    <div className="flex h-full w-full items-center justify-center">
-                        <div className="w-full max-w-xs space-y-4">
-                            <p>With Taskto, every task is an adventure, and you’re the hero</p>
-                            <Button className="cursor-pointer">Join & Play Now</Button>
-                        </div>
+                    <div className="flex-[1]">
+                        <FAQ />
                     </div>
                 </div>
             </section>
