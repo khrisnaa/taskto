cfe30518f84aeb8bb894fb250d48fdeeed1adf9e
khrisnaa <igdkhrisnaaa@gmail.com>
2025-07-26 14:55:45 +0800

    ui: styling landing (mobile)

--- resources/js/components/footer.tsx
+++ resources/js/components/footer.tsx
@@ -1,86 +1,78 @@
-import React from 'react'
-import { Separator } from './ui/separator'
-import { Instagram, Linkedin } from 'lucide-react'
+import { Instagram, Linkedin } from 'lucide-react';
+import { Separator } from './ui/separator';
 
 const Footer = () => {
     const SOCIAL_LINKS = [
         {
-            avatar: '/assets/12.png',
+            avatar: '/assets/13.png',
             name: 'Kemas',
             linkedIn: '#',
             instagram: '#',
         },
         {
-            avatar: '/assets/14.png',
+            avatar: '/assets/12.png',
             name: 'Agung',
             linkedIn: '#',
             instagram: '#',
         },
         {
-            avatar: '/assets/17.png',
+            avatar: '/assets/14.png',
             name: 'Khrisna',
             linkedIn: '#',
             instagram: '#',
         },
         {
-            avatar: '/assets/16.png',
+            avatar: '/assets/11.png',
             name: 'Dede',
             linkedIn: '#',
             instagram: '#',
         },
-    ]
+    ];
 
     return (
-        <div className="mx-auto p-6 bg-secondary rounded-lg shadow-lg">
+        <div className="mx-auto rounded-lg bg-secondary p-6 shadow-lg">
             <div className="lg:flex lg:items-center lg:gap-10">
                 {/* Header Section */}
                 <div className="mb-8 lg:mb-0 lg:flex-1">
-                    <div className="flex justify-start items-center space-x-2 mb-4">
+                    <div className="mb-4 flex items-center justify-start space-x-2">
                         <div className="w-16 md:w-20">
-                            <Separator className='bg-primary' />
+                            <Separator className="bg-primary" />
                         </div>
-                        <img src="/assets/logo.svg" alt="Logo" className="h-6 md:h-8" />
+                        <img src="/assets/logo.svg" alt="Logo" className="h-5" />
                     </div>
 
-                    <h1 className="text-2xl md:text-3xl lg:text-4xl font-bold text-primary mb-4 text-center md:text-left">
-                        Built by Dreamers
-                    </h1>
-                    <p className="text-primary text-sm md:text-base text-center md:text-left max-w-md md:max-w-lg">
-                        Created by a passionate team of developers and creatives who believe task
-                        management should feel like an adventure.
+                    <h1 className="mb-4 text-center text-2xl font-bold text-primary md:text-left md:text-3xl lg:text-4xl">Built by Dreamers</h1>
+                    <p className="max-w-md text-center text-sm text-primary md:max-w-lg md:text-left md:text-base">
+                        Created by a passionate team of developers and creatives who believe task management should feel like an adventure.
                     </p>
                 </div>
 
                 {/* Team Section */}
-                <div className="grid grid-cols-2 gap-4 md:gap-6 lg:flex lg:gap-6 lg:flex-1 lg:justify-between">
+                <div className="grid grid-cols-2 gap-4 sm:flex sm:flex-1 sm:justify-between md:gap-6 lg:gap-6">
                     {SOCIAL_LINKS.map((item, index) => (
-                        <div className="text-center space-y-2 md:space-y-3" key={index}>
-                            <div className="w-16 h-16 md:w-20 md:h-20 lg:w-24 lg:h-24 mx-auto overflow-hidden rounded-full">
-                                <img
-                                    src={item.avatar}
-                                    alt={`${item.name} avatar`}
-                                    className="w-full h-full object-cover"
-                                />
+                        <div className="space-y-2 text-center md:space-y-3" key={index}>
+                            <div className="relative mx-auto flex aspect-square h-12 items-center justify-center overflow-hidden sm:h-16 md:h-24">
+                                <div className="relative aspect-square h-32 overflow-hidden sm:h-40 md:h-64">
+                                    <img src={item.avatar} alt={`${item.name} avatar`} className="h-full w-full object-cover object-center" />
+                                </div>
                             </div>
 
-                            <p className="text-base md:text-lg lg:text-xl font-bold text-primary">
-                                {item.name}
-                            </p>
+                            <p className="text-base font-semibold text-primary md:text-lg lg:text-xl">{item.name}</p>
 
                             <div className="flex justify-center space-x-2 md:space-x-3">
                                 <a
                                     href={item.instagram}
-                                    className="text-primary hover:text-accent transition-colors duration-200"
+                                    className="text-primary transition-colors duration-200 hover:text-accent"
                                     aria-label={`${item.name}'s Instagram`}
                                 >
-                                    <Instagram size={20} className="md:w-6 md:h-6" />
+                                    <Instagram size={20} className="md:h-6 md:w-6" />
                                 </a>
                                 <a
                                     href={item.linkedIn}
-                                    className="text-primary hover:text-accent transition-colors duration-200"
+                                    className="text-primary transition-colors duration-200 hover:text-accent"
                                     aria-label={`${item.name}'s LinkedIn`}
                                 >
-                                    <Linkedin size={20} className="md:w-6 md:h-6" />
+                                    <Linkedin size={20} className="md:h-6 md:w-6" />
                                 </a>
                             </div>
                         </div>
@@ -88,7 +80,7 @@ const Footer = () => {
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default Footer
+export default Footer;
---
+++ resources/js/components/welcome-page/hero-mobile.tsx
@@ -0,0 +1,63 @@
+import { MoveDown } from 'lucide-react';
+import { Button } from '../ui/button';
+import { Separator } from '../ui/separator';
+
+export const HeroMobile = () => {
+    return (
+        <div className="flex h-screen flex-col items-center gap-2 p-8 pb-0">
+            <div className="flex items-center gap-2">
+                <div className="relative size-4">
+                    <img src="/icons/diamonx.svg" alt="Diamond" className="h-full w-full object-cover" />
+                </div>
+                <span className="font-playfair text-2xl font-medium">Taskto</span>
+                <div className="relative size-4">
+                    <img src="/icons/diamonx.svg" alt="Diamond" className="h-full w-full object-cover" />
+                </div>
+            </div>
+            <div className="h-8">
+                <Separator orientation="vertical" className="bg-neutral-600" />
+            </div>
+            <div className="flex flex-col items-center justify-center gap-4 text-center">
+                <h1 className="text-2xl font-semibold">Turn Tasks Into Adventures</h1>
+                <p className="max-w-64 text-sm">With Taskto, every task is an adventure, and you’re the hero</p>
+                <Button>Join & Play Now</Button>
+            </div>
+
+            <div className="relative flex w-full justify-center overflow-hidden">
+                <div className="relative flex flex-col items-center gap-8">
+                    <div className="flex flex-col gap-2 text-xl font-semibold">
+                        <span>タ</span>
+                        <span>ス</span>
+                        <span>ク</span>
+                        <span>ト</span>
+                    </div>
+                    <div className="h-64">
+                        <Separator className="bg-neutral-600" orientation="vertical" />
+                    </div>
+                </div>
+                <div className="relative">
+                    <div className="relative h-[120dvh] -translate-y-[16%] overflow-hidden">
+                        <img alt="Home Image" src="/assets/char2.png" className="h-full w-full object-cover object-top" />
+                    </div>
+                    <div className="absolute bottom-0 left-1/2 flex aspect-square w-[60%] -translate-x-1/2 translate-y-[55%] items-start justify-center rounded-full border border-neutral-600 bg-neutral-100 text-primary">
+                        <div className="flex flex-col items-center justify-center p-5">
+                            <span className="text-lg font-medium">FAQ</span>
+                            <MoveDown className="size-4" />
+                        </div>
+                    </div>
+                </div>
+                <div className="relative flex flex-col items-center gap-8">
+                    <div className="h-64">
+                        <Separator className="bg-neutral-600" orientation="vertical" />
+                    </div>
+                    <div className="flex flex-col gap-2 text-xl font-semibold">
+                        <span>タ</span>
+                        <span>ス</span>
+                        <span>ク</span>
+                        <span>ト</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
